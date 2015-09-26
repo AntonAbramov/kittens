d32c65d9b654cda3ef2cac52eb7350a3af478089
antonabramov <anton@juniqe.com>
2015-09-26 13:05:59 +0200

    update config

--- flightplan.js
+++ flightplan.js
@@ -2,7 +2,7 @@ var plan = require('flightplan');
 
 var appName = 'kittens';
 var username = 'root';
-var startFile = 'bin/server';
+var startFile = 'bin/www';
 
 var tmpDir = appName + '-' + new Date().getTime();
 
@@ -31,19 +31,19 @@ plan.target('production', [
 ]);
 
 // run commands on localhost
-plan.local(function (local) {
+plan.local(function(local) {
   // uncomment these if you need to run a build on your machine first
   // local.log('Run build');
   // local.exec('gulp build');
 
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
-// rsync files to all the destination's hosts
+  // rsync files to all the destination's hosts
   local.transfer(filesToCopy, '/tmp/' + tmpDir);
 });
 
 // run commands on remote hosts (destinations)
-plan.remote(function (remote) {
+plan.remote(function(remote) {
   remote.log('Move folder to root');
   remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
   remote.rm('-rf /tmp/' + tmpDir);
@@ -52,7 +52,7 @@ plan.remote(function (remote) {
   remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
 
   remote.log('Reload application');
-  remote.sudo('ln -snf ~/' + tmpDir + ' ~/' + appName, {user: username});
-  remote.exec('forever stop ~/' + appName + '/' + startFile, {failsafe: true});
-  remote.exec('forever start ~/' + appName + '/' + startFile);
+  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
+  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
+  remote.exec('forever start ~/'+appName+'/'+startFile);
 });
